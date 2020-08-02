aa6bb2c3e7a85431b52325876a1ae056ed2ddc1d
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-08-02 23:17:10 +0200

    better way

--- src/MinecraftTorchCoordinateFinder.php
+++ src/MinecraftTorchCoordinateFinder.php
@@ -8,6 +8,7 @@ class MinecraftTorchCoordinateFinder
     public function run(int $torchLight, int $initialX = 0, int $initialY = 0): array
     {
         $coords = [];
+        
         for ($i = 0; $i < $torchLight; $i++) {
             for ($j = 0; $j < $torchLight - $i; $j++) {
                 $coords[($initialX + $i) . '_' . ($initialY + $j)] = [$initialX + $i, $initialY + $j];
@@ -16,6 +17,7 @@ class MinecraftTorchCoordinateFinder
                 $coords[($initialX - $i) . '_' . ($initialY - $j)] = [$initialX - $i, $initialY - $j];
             }
         }
+
         return array_values($coords);
     }
 }
