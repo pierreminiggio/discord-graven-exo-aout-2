e5a0d6852df2b936b29c5890f74f17c286c00f02
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-08-02 23:14:33 +0200

    better way

--- src/MinecraftTorchCoordinateFinder.php
+++ src/MinecraftTorchCoordinateFinder.php
@@ -5,51 +5,17 @@ namespace PierreMiniggio\MinecraftTorchCoordinateFinder;
 class MinecraftTorchCoordinateFinder
 {
 
-    public static array $cache = [];
-
     public function run(int $torchLight, int $initialX = 0, int $initialY = 0): array
     {
-
-        $cacheKey = $torchLight . '_' . $initialX . '_' . $initialY;
-
-        if (isset(static::$cache[$cacheKey])) {
-            return static::$cache[$cacheKey];
-        }
-
         $coords = [];
-
-        if ($torchLight === 0) {
-            return $coords;
-        }
-
-        $coords[] = [$initialX, $initialY];
-
-        if ($torchLight === 1) {
-            return $coords;
-        }
-
-        $allCoords = array_merge(
-            $this->run($torchLight - 1, $initialX + 1, $initialY),
-            $this->run($torchLight - 1, $initialX - 1, $initialY),
-            $this->run($torchLight - 1, $initialX, $initialY + 1),
-            $this->run($torchLight - 1, $initialX, $initialY - 1)
-        );
-
-        foreach ($allCoords as $newCoord) {
-            $isIn = false;
-
-            foreach ($coords as $coord) {
-                if ($coord[0] === $newCoord[0] && $coord[1] === $newCoord[1]) {
-                    $isIn = true;
-                    break;
-                }
-            }
-
-            if (! $isIn) {
-                $coords[] = $newCoord;
+        for ($i = 0; $i < $torchLight; $i++) {
+            for ($j = 0; $j < $torchLight - $i; $j++) {
+                $coords[($initialX + $i) . '_' . ($initialY + $j)] = [$initialX + $i, $initialY + $j];
+                $coords[($initialX + $i) . '_' . ($initialY - $j)] = [$initialX + $i, $initialY - $j];
+                $coords[($initialX - $i) . '_' . ($initialY + $j)] = [$initialX - $i, $initialY + $j];
+                $coords[($initialX - $i) . '_' . ($initialY - $j)] = [$initialX - $i, $initialY - $j];
             }
         }
-
-        return static::$cache[$cacheKey] = $coords;
+        return array_values($coords);
     }
 }
